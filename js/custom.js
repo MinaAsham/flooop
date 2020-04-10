1be02e789d38b06747e36e4c9d178296dcb43efd
kabilash <kabilash143@gmail.com>
2020-04-11 00:41:24 +0530

    Site completed v1

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,16 @@
 $(function () {
+    if(localStorage.getItem('theme-selected') != null) {
+        let cookieTheme = localStorage.getItem('theme-selected');
+        $('#themeChangingSection').attr('data-theme', cookieTheme);
+        $('.theme-selector-wrapper .theme-name').removeClass('active').parent().find('[data-name='+ cookieTheme +']').addClass('active');
+
+
+    }
+
+    $("#copyrightYear").text( (new Date).getFullYear() );
+
+
+
     $('.hamburger #sideBarMenuIcon').on('click', function () {
         $('.side-bar').toggleClass('shrunk');
     });
@@ -7,6 +19,16 @@ $(function () {
         $('.side-bar').toggleClass('open');
     });
 
+    $('.theme-selector-wrapper #theme-opener-btn').on('click', function () {
+        $('.theme-selector-wrapper').toggleClass('open');
+    });
+
+    $('.theme-name').on('click', function () {
+        let themeName = $(this).data('name');
+        $(this).addClass('active').siblings().removeClass('active');
+        $('#themeChangingSection').attr('data-theme', themeName);
+        localStorage.setItem('theme-selected', themeName);
+    });
 
 });
 
